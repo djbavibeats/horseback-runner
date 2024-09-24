064e6c64f2fb9b93d03de9fdfb70d10e0aa87a9e
djbavibeats <justin@voltcreative.com>
2024-09-24 14:11:08 -0400

    Update page to include instructions

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-    <div className="bottom-0 pb-1">
+    <div className="bottom-0 mt-4">
 
       {/* Mobile Terms */}
       <div className="w-full text-center flex flex-col gap-0 font-snide-asides text-md leading-[1.2rem]">
--- src/Scenes/SplashPage.jsx
+++ src/Scenes/SplashPage.jsx
@@ -5,12 +5,12 @@ import HorsebackLogo from "../../public/images/splash-page/horseback-logo.webp"
 function SplashPage({ setActiveScreen }) {
     return (
         <>
-        <div className="relative flex flex-col items-center gap-2 mt-[-1rem]">
-            <img className="w-[90%]" src={ JennaPauletteStackedLogo } />
+        <div className="relative flex flex-col items-center gap-2 mt-[-2rem]">
+            <img className="w-[80%]" src={ JennaPauletteStackedLogo } />
             <p className="font-snide-asides text-2xl">And</p>
-            <img className="w-[90%]" src={ JustinBootsLogo } />
+            <img className="w-[80%]" src={ JustinBootsLogo } />
             <p className="font-snide-asides text-2xl">Present</p>
-            <img className="w-[90%]" src={ HorsebackLogo } />
+            <img className="w-[80%]" src={ HorsebackLogo } />
             <div className="special-button w-[170px] mt-2 animate-bounce" onClick={ () => setActiveScreen('instructions')}>
                 <p className="font-snide-asides text-2xl">Click To Begin</p>
             </div>
