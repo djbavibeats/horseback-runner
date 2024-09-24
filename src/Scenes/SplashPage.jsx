99a6804e849d9044eb32843e579fbb8c84cbcabe
djbavibeats <justin@voltcreative.com>
2024-09-24 14:16:18 -0400

    Update page to include instructions

--- src/Scenes/SplashPage.jsx
+++ src/Scenes/SplashPage.jsx
@@ -5,7 +5,7 @@ import HorsebackLogo from "../../public/images/splash-page/horseback-logo.webp"
 function SplashPage({ setActiveScreen }) {
     return (
         <>
-        <div className="relative flex flex-col items-center gap-2 mt-[-2rem]">
+        <div className="relative flex flex-col w-full h-full items-center justify-center gap-4">
             <img className="w-[80%]" src={ JennaPauletteStackedLogo } />
             <p className="font-snide-asides text-2xl">And</p>
             <img className="w-[80%]" src={ JustinBootsLogo } />
